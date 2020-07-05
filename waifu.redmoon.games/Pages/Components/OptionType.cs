e86714cda9ed04eea680cb1ce3ce29d9dec1a3a5
redmoongames <redmoon.thebest@gmail.com>
2020-07-05 15:44:11 +0300

    Initial commit

---
+++ waifu.redmoon.games/Pages/Components/OptionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace waifu.redmoon.games.Pages.Components
+{
+    public enum OptionType
+    {
+        analytics,
+        clicker,
+        account,
+        none
+    }
+}
